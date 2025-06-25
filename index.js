415cf884caa984d8151b3a6da225a806cfe4a8d1
Suleiman Aziz <Suleimanaziz212@gmail.com>
2025-06-25 22:07:06 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+      const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, string){
+    return drivers.filter( driver => {
+        return driver.toLowerCase() === string.toLowerCase()});
+};
+function fuzzyMatch(drivers, string){
+   
+  return drivers.filter(driver => {
+    return driver.toLowerCase().startsWith(string.toLowerCase())}
+  );
+}
+function matchName(drivers, string) {
+  return drivers.filter(driver =>{
+    return driver.name.toLowerCase() === string.toLowerCase()
+});
+}
+
